54d97f9bdc48ca2b21792d17495fe376adff26ff
pollyanafontessei <pollysfontesseixas@gmail.com>
2024-03-26 21:42:01 -0300

    ﻿

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'igjtof',
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true, //gráficos
+      reportTitle: 'Projeto Pollyana Cypress', //nome do relatório
+      reportPageTitle: 'Projeto Pollyana Cypress', //titulo da página
+    },
     baseUrl:"https://automationpratice.com.br/",
     defaultCommandTimeout:8000,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
